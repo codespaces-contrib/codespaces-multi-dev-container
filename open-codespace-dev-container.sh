873dfb334342ae9a7ffea7e3b67e4a513d0d47a8
Chuck Lantz <clantz@microsoft.com>
2021-10-21 14:47:01 -0700

    Unify

--- open-codespace-dev-container.sh
+++ open-codespace-dev-container.sh
@@ -25,13 +25,6 @@ if [ ! -z "${COMPOSE_PROJECT_NAME}" ]; then
     export COMPOSE_PROJECT_NAME
 fi
 
-# Find the workspace mount point on the host
-workspace_mount_source="$(docker inspect -f '{{range .HostConfig.Mounts}}{{if eq .Target "/workspaces"}}{{.Source}}{{end}}{{end}}' ${bootstrap_container})"
-if [ -z "${workspace_mount_source}" ]; then
-    workspace_mount_source="${CODESPACES_DEFAULT_WORKSPACE_MOUNT}"
-fi
-echo "Workspace mount source: ${workspace_mount_source}"
-
 # Determine where workspace exists in boostrap container
 if [ "${workspace_folder_in_container}" = "unknown" ]; then
     workspace_folder_in_container="$(docker exec -i ${bootstrap_container} /bin/sh -c 'cat /tmp/__boostrap_container_workspace_folder')"
@@ -58,12 +51,20 @@ while IFS= read -r content_path; do
 done < common-config.list
 echo "- ${devcontainer_relative_path}/.devcontainer"
 docker cp -L "${bootstrap_container}:${workspace_folder_in_container}/${devcontainer_relative_path}/.devcontainer" "${temp_devcontainer_folder}"
+echo
 
 # Remove comments given devcontainer.json is a jsonc file
 temp_evcontainerjson_file="${temp_devcontainer_folder}/.devcontainer/devcontainer.json"
 sed -i'.bak' -e "s/\\/\\/.*/ /g" "${temp_evcontainerjson_file}"
 # Append workspace mount property if not a docker compose definition
 if ! jq -e '.dockerComposeFile' "${temp_evcontainerjson_file}" >/dev/null 2>&1; then
+    # Find the workspace mount point on the host
+    workspace_mount_source="$(docker inspect -f '{{range .HostConfig.Mounts}}{{if eq .Target "/workspaces"}}{{.Source}}{{end}}{{end}}' ${bootstrap_container})"
+    if [ -z "${workspace_mount_source}" ]; then
+        workspace_mount_source="${CODESPACES_DEFAULT_WORKSPACE_MOUNT}"
+    fi
+    echo "Workspace mount source: ${workspace_mount_source}"
+    # Update devcontainer.json
     jq  --arg workspaceFolder "${workspace_folder_in_container}/${devcontainer_relative_path}" \
         --arg workspaceMount "source=${workspace_mount_source},destination=/workspaces,type=bind" \
         '.workspaceFolder = $workspaceFolder | .workspaceMount = $workspaceMount' \
@@ -71,14 +72,16 @@ if ! jq -e '.dockerComposeFile' "${temp_evcontainerjson_file}" >/dev/null 2>&1; 
     mv -f "${temp_evcontainerjson_file}.new" "${temp_evcontainerjson_file}"
 fi
 
-# Remotely build dev container better perf
-echo "Building dev container..."
-docker exec -i ${bootstrap_container} /bin/sh -c "\
-    cd ${workspace_folder_in_container}/${devcontainer_relative_path}; \
-    devcontainer build ."
+# Remotely build dev container for better perf if boostrap is not part of a compose
+# definition (since in this case, the containers would already be built)
+if [ -z "${COMPOSE_PROJECT_NAME}" ]; then
+    echo "Building dev container..."
+    docker exec -i ${bootstrap_container} /bin/sh -c "\
+        cd ${workspace_folder_in_container}/${devcontainer_relative_path}; \
+        devcontainer build ."
+fi
 
 # Launch VS Code
-echo
 echo "Launching VS Code..."
 code --force-user-env --disable-workspace-trust --skip-add-to-recently-opened "${temp_devcontainer_folder}"
 echo "Done!"
