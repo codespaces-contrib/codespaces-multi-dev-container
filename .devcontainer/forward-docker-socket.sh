5c9f12ea9d73462414e13d06a9a85e86e4817604
Chuck Lantz <chuck_lantz@hotmail.com>
2021-10-22 18:42:32 +0000

    Fix

--- .devcontainer/forward-docker-socket.sh
+++ .devcontainer/forward-docker-socket.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 SOURCE_SOCKET="/var/run/docker.sock"
 TARGET_PORT="9256"
 
 # Wrapper function to only use sudo if not already root
-sudoIf()
-{
-    if [ "$(id -u)" -ne 0 ]; then
-        sudo "$@"
-    else
-        "$@"
-    fi
-}
+sudoIf=""
+if [ "$(id -u)" -ne 0 ]; then
+    sudoIf="sudo"
+fi
 
 # Set up socket forwader
-sudoIf nohup bash -c "socat TCP-LISTEN:${TARGET_PORT},fork,bind=127.0.0.1 UNIX-CONNECT:${SOURCE_SOCKET} > /tmp/socat.log 2>&1 &" > /dev/null 2>&1
+nohup bash -c "${sudoIf} socat -d -d TCP-LISTEN:${TARGET_PORT},fork,bind=127.0.0.1 UNIX-CONNECT:${SOURCE_SOCKET} > /tmp/socat.log 2>&1 &" > /dev/null 2>&1
 
 set +e
 exec "$@"
